a48dcf94d7343b70bd9b50d418fcba0a8f7e4c31
Amanuel <amanueleman@gmail.com>
2023-12-12 23:15:54 -0500

    a function to add new employee is added

--- server.js
+++ server.js
@@ -181,17 +181,92 @@ const addNewRole = () => {
         ];
 
         inquier.prompt(questions).then(response => {
-                const query = `INSERT INTO role (title, salary, department_id) VALUES (?)`;
-                db.query(query, [[response.title, response.salary, response.department]], (err, res) => {
-                    if (err) throw err;
-                    console.log(`Successfully added ${response.title} role with id ${res.insertId}`);
+            const query = `INSERT INTO role (title, salary, department_id) VALUES (?)`;
+            db.query(query, [[response.title, response.salary, response.department]], (err, res) => {
+                if (err) throw err;
+                console.log(`Successfully added ${response.title} role with id ${res.insertId}`);
 
-                    startPrompt();
-                });
-            })
+                startPrompt();
+            });
+        })
             .catch(err => {
                 console.error(err);
             });
 
     });
 }
+
+// function to add new employee
+const addNewEmployee = () => {
+    //get all the employee list to make choice of employee's manager
+    db.query("SELECT * FROM employee", (err, res) => {
+        if (err) throw err;
+
+        //an employee could have no manager
+        const employeeChoice = [
+            {
+                name: 'None',
+                value: 0
+            }
+        ]; 
+
+        res.forEach(({ first_name, last_name, id }) => {
+            employeeChoice.push({
+                name: first_name + " " + last_name,
+                value: id
+            });
+        });
+
+        //get all the role list to make choice of employee's role
+        db.query("SELECT * FROM role", (err, res) => {
+            if (err) throw err;
+            const roleChoice = [];
+            res.forEach(({ title, id }) => {
+                roleChoice.push({
+                    name: title,
+                    value: id
+                });
+            });
+
+            let questions = [
+                {
+                    type: "input",
+                    name: "first_name",
+                    message: "what is the employee's first name?"
+                },
+                {
+                    type: "input",
+                    name: "last_name",
+                    message: "what is the employee's last name?"
+                },
+                {
+                    type: "list",
+                    name: "role_id",
+                    choices: roleChoice,
+                    message: "what is the employee's role?"
+                },
+                {
+                    type: "list",
+                    name: "manager_id",
+                    choices: employeeChoice,
+                    message: "who is the employee's manager? (could be null)"
+                }
+            ]
+
+            inquier.prompt(questions)
+                .then(response => {
+                    const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?)`;
+                    let manager_id = response.manager_id !== 0 ? response.manager_id : null;
+                    db.query(query, [[response.first_name, response.last_name, response.role_id, manager_id]], (err, res) => {
+                        if (err) throw err;
+                        console.log(`successfully inserted employee ${response.first_name} ${response.last_name} with id ${res.insertId}`);
+                        
+                        startPrompt();
+                    });
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        })
+    });
+} 
